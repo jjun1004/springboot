9da861b127fb2cac6267631d0b630615ef827fdd
junki park <pjk146195@gmail.com>
2021-10-15 18:22:50 +0900

    1015 springboot day2 끝

--- src/main/java/com/mycompany/webapp/controller/ThymeleafController.java
+++ src/main/java/com/mycompany/webapp/controller/ThymeleafController.java
@@ -2,6 +2,8 @@ package com.mycompany.webapp.controller;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
+
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +20,7 @@ public class ThymeleafController {
 	@RequestMapping("/content")
 	public String content() {
 		log.info("실행");
-		return "/thymeleaf/content";
+		return "thymeleaf/content";
 	}
 	
 	@RequestMapping("/text")
@@ -50,9 +52,40 @@ public class ThymeleafController {
 		model.addAttribute("board", board);
   
 		JSONObject jsonObject = new JSONObject(board);
-		model.addAttribute("jsonBoard", jsonObject.toString());
+		model.addAttribute("jsonBoard", jsonObject.toString()); // request범위에 저장
 		
 		return "thymeleaf/javascript";
 		
+	}
+	
+	@RequestMapping("/variableExpressions")
+	public String variableExpressions(HttpSession session) {
+		log.info("실행");
+		if(session.getAttribute("sessionMid") == null) {
+			session.setAttribute("sessionMid", "thymeleaf");
+		} else {
+			session.removeAttribute("sessionMid");
+		}
+		
+		return "thymeleaf/variableExpressions";
+	}
+	
+	@RequestMapping("selectionVariableExpressions")
+	public String selectionVariableExpressions(Model model) {
+	      log.info("실행");
+	      Board board = new Board();
+	      board.setBno(1);
+	      board.setBtitle("Spring Boot Template Engine");
+	      board.setBcontent("<span style='color:red'>Thymeleaf</span> is a modern server-side <b>Java template engine</b>");
+	      board.setMid("thymeleaf");
+	      board.setBdate(new Date());
+	      model.addAttribute("board", board);
+	      return "thymeleaf/selectionVariableExpressions";
+	}
+	
+	@RequestMapping("messageExpressions")
+	public String messageExpressions() {
+		log.info("실행");
+		return "thymeleaf/messageExpressions";
 	}
 }
