86e01e80dc59d04a62586fd4a68b71ccce43c5dc
jun ki <pjk146195@gmail.com>
2021-10-19 14:37:38 +0900

    1019 springboot day4

--- src/main/java/com/mycompany/webapp/dto/Member.java
+++ src/main/java/com/mycompany/webapp/dto/Member.java
@@ -7,7 +7,7 @@ public class Member {
 	private String mid;
 	private String mname;
 	private String mpassword;
-	private int menabled;
+	private boolean menabled; // boolean으로 바꾸면mybatis에서 자동으로 1은 true, 0은 false로 매핑해줌.
 	private String mrole;
 	private String memail;
 }
